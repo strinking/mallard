c1c2b39af4a1e2b2d094b8ffad411f50e8b8c9d2
Volcyy <Johannes.christ@gmail.com>
2017-08-23 17:20:26 +0200

    Initial working version with search.

--- mallard/__main__.py
+++ mallard/__main__.py
@@ -6,15 +6,16 @@ mallard by using
 from the parent directory.
 """
 
-from discord.ext import commands
+import html
+import json
+
+import duckduckpy
+import discord
 
 from .util import CONFIG
 
 
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(CONFIG['prefix']),
-    description=CONFIG['description']
-)
+bot = discord.Client()
 
 
 @bot.event
@@ -26,5 +27,48 @@ async def on_ready():
 
     print("Logged in.")
 
+
+@bot.event
+async def on_message(msg):
+    """
+    This coroutine is called when a message
+    is received. Basically, this checks if
+    the bot was mentioned and if so, will
+    query the DuckDuckGo API for the message
+    content without any mentions.
+    """
+
+    content_list = msg.content.split()
+
+    # The Message is an Embed or an Image
+    if not content_list:
+        return
+
+    mention_id_string = ''.join(c for c in content_list[0] if c.isdigit())
+    # The Bot was not mentioned
+    if not mention_id_string or int(''.join(mention_id_string)) != bot.user.id:
+        return
+
+    query_string = ''.join(content_list[1:])
+    resp = duckduckpy.query(query_string)
+
+    if not resp.abstract_source or not resp.related_topics:
+        await msg.channel.send(embed=discord.Embed(
+            title="Sorry, no results.", colour=discord.Colour.orange()
+        ))
+    else:
+        embed = discord.Embed(colour=discord.Colour.orange())
+        embed.set_author(
+            name=f"DuckDuckGo: {resp.heading}",
+            url=f"https://duckduckgo.com/?q={html.escape(query_string)}"
+        )
+        for result in resp.related_topics:
+            if hasattr(result, 'first_url'):
+                url = result.first_url + '#' if result.first_url.endswith(')') else result.first_url
+                embed.add_field(
+                    name=url,
+                    value=result.text
+                )
+        await msg.channel.send(embed=embed)
 
 bot.run(CONFIG['discord_token'])
