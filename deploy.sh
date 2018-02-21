6d50a4ec2a9772908c609a7fc6cd94416d5ea2c1
Ammon Smith <ammon.i.smith@gmail.com>
2018-02-21 03:31:50 +0000

    Move systemd unit directory location.

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,17 @@ fi
 repo_dir="$(dirname "$0")"
 dest_dir=~mallard/repo
 
-rm -r "$dest_dir"
+rm -rf "$dest_dir"
 mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
 install -m400 "$1" "$dest_dir/config.yaml"
 chown -R mallard:mallard "$dest_dir"
 echo "Installed source code to '$dest_dir'"
 
-python3.6 -m pip install -r "$repo_dir/requirements.txt" > /dev/null
+python3.6 -m pip install -r "$repo_dir/requirements.txt"
 echo "Installed Python dependencies"
 
-install -m644 "$repo_dir/misc/mallard.service" /etc/systemd/system/mallard.service
+install -m644 "$repo_dir/misc/mallard.service" /usr/local/lib/systemd/system/mallard.service
 chown root:root /etc/systemd/system/mallard.service
 echo "Installed systemd service"
 
